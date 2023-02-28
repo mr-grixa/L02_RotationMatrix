6fe267ab246bcbf88232d32e135cc502c8115123
mr-grixa <grixa2403@gmail.com>
2023-03-01 01:54:34 +0300

    Добавьте файлы проекта.

---
+++ L02_RotationMatrix/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace L02_RotationMatrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpDownCout = new System.Windows.Forms.NumericUpDown();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownCout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UpDownCout
+            // 
+            this.UpDownCout.Location = new System.Drawing.Point(150, 17);
+            this.UpDownCout.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.UpDownCout.Name = "UpDownCout";
+            this.UpDownCout.Size = new System.Drawing.Size(120, 20);
+            this.UpDownCout.TabIndex = 3;
+            this.UpDownCout.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(12, 12);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(131, 26);
+            this.buttonGenerate.TabIndex = 2;
+            this.buttonGenerate.Text = "Сгенерировать";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(13, 44);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(131, 26);
+            this.buttonLoad.TabIndex = 15;
+            this.buttonLoad.Text = "Загрузить";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(150, 44);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(131, 26);
+            this.buttonSave.TabIndex = 16;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(307, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(48, 225);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 18;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 618);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.UpDownCout);
+            this.Controls.Add(this.buttonGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownCout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown UpDownCout;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ L02_RotationMatrix/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L02_RotationMatrix
+{
+    public partial class Form1 : Form
+    {
+        List<Tuple<double, double, double>> XYZPoints;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            XYZPoints = GenerateRandomPoints((int)UpDownCout.Value);
+            DrawPerspective();
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Табличные данные (*.CSV)|*.CSV";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                XYZPoints = LoadDataFromCSV(openFileDialog.FileName);
+
+            }
+        } 
+        
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Табличные данные (*.CSV)|*.CSV";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SaveDataToCSV(saveFileDialog.FileName, XYZPoints);
+            }
+        }
+
+        private void DrawOrthogonal()
+        {
+            Bitmap bitmap = new Bitmap(600, 600);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.Clear(Color.MidnightBlue);
+            }
+            for (int i = 0; i < XYZPoints.Count; i++)
+            {
+                int px = (int)Math.Round((XYZPoints[i].Item1 + 1) / 2 * (bitmap.Width-1));
+                int py = (int)Math.Round((XYZPoints[i].Item2 + 1) / 2 * (bitmap.Height-1));
+
+                int colorValue = (int)Math.Round(255 * (XYZPoints[i].Item3 + 1) / 2);
+                Color color = Color.FromArgb(colorValue, colorValue, colorValue);
+                bitmap.SetPixel(px, py, color);
+            }
+            pictureBox1.Image = bitmap;
+
+        }
+        private void DrawPerspective()
+        {
+            Bitmap bitmap = new Bitmap(600, 600);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.Clear(Color.MidnightBlue);
+            }
+            double theta = Math.PI / 4; // угол обзора в радианах
+            for (int i = 0; i < XYZPoints.Count; i++)
+            {
+                double xNew = Math.Cos(theta) * XYZPoints[i].Item1 + Math.Sin(theta) * XYZPoints[i].Item3;
+                double yNew = XYZPoints[i].Item2;
+                double zNew = -Math.Sin(theta) * XYZPoints[i].Item1 + Math.Cos(theta) * XYZPoints[i].Item3;
+
+                // Вычисляем яркость точки
+                int colorValue = (int)Math.Round(255 * (XYZPoints[i].Item3 + 1) / 2);
+                Color color = Color.FromArgb(colorValue, colorValue, colorValue);
+
+                // Рисуем точку на битовой карте с заданной яркостью
+                int xPixel = (int)((xNew + 1) / 2 * bitmap.Width);
+                int yPixel = (int)((yNew+1) / 2 * bitmap.Height);
+                bitmap.SetPixel(xPixel, yPixel, color);
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        static void SaveDataToCSV(string filename, List<Tuple<double, double, double>> data)
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                foreach (Tuple<double, double, double> point in data)
+                {
+                    writer.WriteLine($"{point.Item1};{point.Item2};{point.Item3}");
+                }
+            }
+        }
+
+        private List<Tuple<double, double, double>> LoadDataFromCSV(string filename)
+        {
+            List<Tuple<double, double, double>> data = new List<Tuple<double, double, double>>();
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(';');
+                    double x = double.Parse(values[0]);
+                    double y = double.Parse(values[1]);
+                    double z = double.Parse(values[2]);
+                    data.Add(new Tuple<double, double, double>(x, y, z));
+                }
+            }
+            return data;
+        }
+
+        private List<Tuple<double, double, double>> GenerateRandomPoints(int numPoints)
+        {
+            Random random = new Random();
+            List<Tuple<double, double, double>> points = new List<Tuple<double, double, double>>();
+            for (int i = 0; i < numPoints; i++)
+            {
+                double x = random.NextDouble() * 2 - 1; // случайное число в диапазоне [-1, 1)
+                double y = random.NextDouble() * 2 - 1; // случайное число в диапазоне [-1, 1)
+                double z = random.NextDouble() * 2 - 1; // случайное число в диапазоне [-1, 1)
+                points.Add(new Tuple<double, double, double>(x, y, z));
+            }
+            return points;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = Math.Sin((double)numericUpDown1.Value).ToString();
+        }
+    }
+}
